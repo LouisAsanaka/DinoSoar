87020b8728305950247df666158db203920c4f88
Louis Asanaka <25756888+LouisAsanaka@users.noreply.github.com>
2021-04-27 11:41:57 +0800

    feat: made new environment the default

--- train_dino.py
+++ train_dino.py
@@ -1,155 +1,205 @@
-from dinoenv.chrome_dino_env import ChromeDinoEnv
-from dinoenv.wrappers import WarpFrame
+import numpy as np
+import os
+import gym
+from gym import error, spaces
+from collections import deque
 
-from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv, SubprocVecEnv
-from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.callbacks import CheckpointCallback
-from stable_baselines3.common.evaluation import evaluate_policy
+from io import BytesIO
+from PIL import Image
+import base64
+import cv2
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import time
+
+class ChromeDinoEnv(gym.Env):
+
+    def __init__(self,
+            screen_width: int=120,
+            screen_height: int=120,
+            chromedriver_path: str="chromedriver"
+        ):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.chromedriver_path = chromedriver_path
+
+        self.action_space = spaces.Discrete(3) # do nothing, up, down
+        self.observation_space = spaces.Box(
+            low=0, 
+            high=255, 
+            shape=(self.screen_width, self.screen_height, 4), 
+            dtype=np.uint8
+        )
+
+        _chrome_options = Options()
+        _chrome_options.add_argument('--disable-infobars')
+        _chrome_options.add_argument('--mute-audio')
+        _chrome_options.add_argument('--no-sandbox')
+        _chrome_options.add_argument('--window-size=1200,800')
+        _chrome_options.add_argument('--headless')
+
+        self._driver = webdriver.Chrome(
+            executable_path=self.chromedriver_path,
+            options=_chrome_options
+        )
+        self._driver.get('https://wayou.github.io/t-rex-runner')
+        WebDriverWait(self._driver, 10).until(
+            EC.presence_of_element_located((
+                By.CLASS_NAME, 
+                "runner-canvas"
+            ))
+        )
+        self.set_parameter('ACCELERATION', 0.0)
+        self.set_parameter('BG_CLOUD_SPEED', 0.0)
+        self.set_parameter('CLOUD_FREQUENCY', 0.0)
+        self.set_parameter('INVERT_DISTANCE', 1000000000)
+        self.set_parameter('MAX_CLOUDS', 0)
+
+        self.current_key = None
+        self.state_queue = deque(maxlen=4)
+
+        self.actions_map = [
+            Keys.ARROW_RIGHT, # do nothing
+            Keys.ARROW_UP, # jump
+            Keys.ARROW_DOWN # duck
+        ]
+        action_chains = ActionChains(self._driver)
+        self.keydown_actions = [action_chains.key_down(item) for item in self.actions_map]
+        self.keyup_actions = [action_chains.key_up(item) for item in self.actions_map]
+
+    def set_parameter(self, key, value):
+        self._driver.execute_script(f"Runner.config.{key} = {value};")
+
+    def reset(self):
+        # trigger game start
+        self._driver.find_element_by_tag_name("body").send_keys(Keys.SPACE)
+        return self._next_observation()
+
+    def _get_image(self):
+        LEADING_TEXT = "data:image/png;base64,"
+        _img = self._driver.execute_script(
+            "return document.querySelector('canvas.runner-canvas').toDataURL()"
+        )
+        _img = _img[len(LEADING_TEXT):]
+        return np.array(
+            Image.open(BytesIO(base64.b64decode(_img)))
+        )
+
+    def _next_observation(self):
+        image = cv2.cvtColor(self._get_image(), cv2.COLOR_BGR2GRAY)
+        image = image[:500, :480] # cropping
+        image = cv2.resize(image, (self.screen_width, self.screen_height))
+
+        self.state_queue.append(image)
+
+        if len(self.state_queue) < 4:
+            return np.stack([image] * 4, axis=-1)
+        else:
+            return np.stack(self.state_queue, axis=-1)
+
+        return image
+
+    def _get_score(self):
+        return int(''.join(
+            self._driver.execute_script("return Runner.instance_.distanceMeter.digits")
+        ))
+
+    def _get_done(self):
+        return not self._driver.execute_script("return Runner.instance_.playing")
+
+    def step(self, action: int):
+        self._driver.find_element_by_tag_name("body") \
+            .send_keys(self.actions_map[action])
+
+        obs = self._next_observation()
+
+        done = self._get_done()
+        reward = .1 if not done else -1
+
+        time.sleep(.015)
+
+        return obs, reward, done, {"score": self._get_score()}
+
+    def render(self, mode: str='human'):
+        img = cv2.cvtColor(self._get_image(), cv2.COLOR_BGR2RGB)
+        if mode == 'rgb_array':
+            return img
+        elif mode == 'human':
+            from gym.envs.classic_control import rendering
+            if self.viewer is None:
+                self.viewer = rendering.SimpleImageViewer()
+            self.viewer.imshow(img)
+            return self.viewer.isopen
+
+    def close(self):
+        if self.viewer is not None:
+            self.viewer.close()
+            self.viewer = None
 
 import imageio
-import numpy as np
-import sys
-from random import choice
-from string import ascii_lowercase
-from typing import Callable
 
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.callbacks import CheckpointCallback
 
-def random_model_name():
-    return ''.join(choice(ascii_lowercase) for i in range(6))
-
-
-def warp_frame(env):
-    return WarpFrame(env, 160, 80)
-
-
-def linear_schedule(initial_value: float) -> Callable[[float], float]:
-    """
-    Linear learning rate schedule.
-
-    :param initial_value: Initial learning rate.
-    :return: schedule that computes
-      current learning rate depending on remaining progress
-    """
-    def func(progress_remaining: float) -> float:
-        """
-        Progress will decrease from 1 (beginning) to 0.
-
-        :param progress_remaining:
-        :return: current learning rate
-        """
-        return progress_remaining * initial_value
-
-    return func
-
-
-def train(timesteps: int, save_freq: int, prev_model: str = None):
-    print("> Creating environment...")
-    vec_env: SubprocVecEnv = make_vec_env(
-        ChromeDinoEnv, n_envs=8, wrapper_class=warp_frame,
-        env_kwargs={'render': False, 'accelerate': False, 'autoscale': False},
-        vec_env_cls=SubprocVecEnv
+if __name__ == '__main__':
+    env_lambda = lambda: ChromeDinoEnv(
+        screen_width=96,
+        screen_height=96,
+        chromedriver_path=os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            "chromedriver"
+        )
     )
-    vec_env.env_method(method_name='set_default_parameters')
+    do_train = False
+    num_cpu = 1
+    save_path = "chrome_dino_ppo_cnn"
+    env = SubprocVecEnv([env_lambda for i in range(num_cpu)])
 
-    env = VecFrameStack(vec_env, n_stack=4)
-
-    model = None
-    if prev_model is not None:
-        print("> Loading previous model...")
-        model = PPO.load(prev_model, env, learning_rate=3e-4, 
-            verbose=1, tensorboard_log="./ppo_dino_tensorboard/")
-    else:
-        print("> Creating model...")
-        model = PPO("CnnPolicy", env, learning_rate=3e-4, 
-            verbose=1, tensorboard_log="./ppo_dino_tensorboard/")
-    
-    model_name = random_model_name()
-    save_path = f"ppo_dino_{model_name}"
-    print(f"> Model name: {model_name}")
-    print("> Learning...")
-    try:
-        # Save a checkpoint every few steps
+    if do_train:
         checkpoint_callback = CheckpointCallback(
-            save_freq=save_freq, save_path=f"./checkpoints/{model_name}", name_prefix=model_name)
-
-        model.learn(total_timesteps=timesteps, callback=checkpoint_callback)
-        print(f"> Done! Saving as {save_path}.zip")
+            save_freq=200000, 
+            save_path='./checkpoints/',
+            name_prefix=save_path,
+        )
+        model = PPO(
+            'CnnPolicy',
+            env,
+            verbose=2,
+            tensorboard_log="./tb_chromedino_env/",
+        )
+        model.learn(
+            total_timesteps=2000000, 
+            callback=[checkpoint_callback]
+        )
         model.save(save_path)
-    except KeyboardInterrupt:
-        print("> Quitting prematurely...")
-        print(f"> Saving as {save_path}.zip")
-        model.save(save_path)
-        sys.exit(0)
-
-
-def evaluate(model_file):
-    print("> Creating environment...")
-    env: DummyVecEnv = make_vec_env(
-        ChromeDinoEnv, n_envs=1, wrapper_class=warp_frame,
-        env_kwargs={'render': True, 'accelerate': False, 'autoscale': False}
-    )
-
-    unwrapped_game = env.envs[0].game
-
-    # Constant speed training first
-    unwrapped_game.set_parameter('config.ACCELERATION', 0)
-    unwrapped_game.set_parameter('config.BG_CLOUD_SPEED', 0.0)
-    unwrapped_game.set_parameter('config.CLOUD_FREQUENCY', 0.0)
-    unwrapped_game.set_parameter('config.INVERT_DISTANCE', 1000000000)
-    unwrapped_game.set_parameter('config.MAX_CLOUDS', 0)
-
-    env = VecFrameStack(env, n_stack=4)
-
-    print("> Loading model")
-    model = PPO.load(model_file)
+    
+    model = PPO.load(save_path, env=env)
 
     images = []
-    dones = np.array([False])
+
     obs = env.reset()
-    img = env.render(mode='rgb_array')
-    reward_total = 0.0
+    dones = np.array([False] * num_cpu)
+    img = model.env.render(mode='rgb_array')
+
+    i = 0
     while not np.all(dones):
         images.append(img)
         action, _states = model.predict(obs, deterministic=True)
-        print(action)
         obs, rewards, dones, info = env.step(action)
-        reward_total += rewards[0]
+
+        # env.render(mode='human')
+        
         img = env.render(mode='rgb_array')
-    print(f"Reward: {reward_total}")
+        i += 1
+
     imageio.mimsave('dino.gif', [np.array(img) for i, img in enumerate(images)], fps=15)
-    env.close()
 
-def actual_eval(model_file):
-    print("> Creating environment...")
-    env: DummyVecEnv = make_vec_env(
-        ChromeDinoEnv, n_envs=1, wrapper_class=warp_frame,
-        env_kwargs={'render': False, 'accelerate': False, 'autoscale': False}
-    )
-
-    unwrapped_game = env.envs[0].game
-
-    # Constant speed training first
-    unwrapped_game.set_parameter('config.ACCELERATION', 0)
-    unwrapped_game.set_parameter('config.BG_CLOUD_SPEED', 0.0)
-    unwrapped_game.set_parameter('config.CLOUD_FREQUENCY', 0.0)
-    unwrapped_game.set_parameter('config.INVERT_DISTANCE', 1000000000)
-    unwrapped_game.set_parameter('config.MAX_CLOUDS', 0)
-
-    env = VecFrameStack(env, n_stack=4)
-
-    print("> Loading model")
-    model = PPO.load(model_file)
-
-    print("> Evaluating...")
-    mean, std = evaluate_policy(model, env, n_eval_episodes=10, deterministic=True, render=False, return_episode_rewards=False, warn=True)
-    print(f"> Mean: {mean} | Std: {std}")
-
-# Run tensorboard remote: https://stackoverflow.com/a/40413202/11323583
-def main():
-    # train(timesteps=6000000, save_freq=60000)#, prev_model='nwjhlr_800000_steps.zip')
-    # evaluate('timqkc_1800000_steps.zip')
-    # actual_eval('ppo_dino_xbfgep.zip')
-
-if __name__ == "__main__":
-    main()
+    exit()
